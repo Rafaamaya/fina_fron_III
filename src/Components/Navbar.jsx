83d21a37fade1591533127d17fdd291f508dbcc4
Rafael amaya <rafa.amaya@gmail.com>
2023-04-04 00:08:56 -0300

    se agregaron los links al Nabar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
-
   return (
-    <nav>
+    <nav className="nav">
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+      <Link to="/home">Home</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="favs">Favs</Link>
+      <button className="favButton">Change theme</button>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
