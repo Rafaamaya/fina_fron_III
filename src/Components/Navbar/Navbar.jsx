41604566ef6a16abdfc74ce416027d560f57e886
Rafael amaya <rafa.amaya@gmail.com>
2023-04-09 19:02:05 -0300

    se agrego material UI

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,32 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { DarkModeContext } from "../../context/DarkModeContext";
+import LogoutIcon from "@mui/icons-material/Logout";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
   const { isDarkMode, handleMode } = useContext(DarkModeContext);
-  
-  const handleDarkMode = () =>{
+
+  const handleDarkMode = () => {
     handleMode();
-  }
+  };
   return (
-    <nav className={isDarkMode? "dark" : null}>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
+    <nav className={isDarkMode ? "dark" : null}>
       <Link to="/">Home</Link>
       <Link to="/contact">Contact</Link>
       <Link to="/favs">Favs</Link>
-      <button onClick={handleDarkMode}>Change theme</button>
+      <DarkModeIcon
+        className="darkthemeicon"
+        alt="darkmodeicon"
+        onClick={handleDarkMode}
+      />
+      <LogoutIcon
+        className="logout"
+        alt="logouticon"
+        onClick={handleDarkMode}
+      />
     </nav>
   );
 };
